721d77af1bd2d00778a1881edc6ffe0b8e181e84
Oliver <ljyou001@users.noreply.github.com>
2017-04-20 11:54:15 -0500

    Programming For Question 2

--- Assignment2/q2/ContentSorter.java
+++ Assignment2/q2/ContentSorter.java
@@ -3,11 +3,33 @@ import java.io.*;
 public class ContentSorter extends AbsContentSorter
 {
 	public ContentSorter() throws IOException 
+	{}
+	public void sort() throws IOException
 	{
-		
+		buffer[0] = getInt(0);
+		buffer[1] = getInt(1);
+		buffer[2] = getInt(2);
+		buffer[3] = getInt(3);
+		buffer[4] = getInt(4);
+		while(buffer[0] != -1 && buffer[1] != -1 && buffer[2] != -1 && buffer[3] != -1 && buffer[4] != -1)
+		{
+			int c = findMinIndex(buffer);
+			output(buffer[c]);
+			buffer[c] = getInt(c);
+		}
 	}
-	public void sort()
+	private int findMinIndex(int [] a)
 	{
-		return;
+		int min = a[0];
+		int count = 0;
+		for (int i = 1; i < a.length; i++)
+		{
+			if (a[i] < min)
+			{
+				min = a[i];
+				count = i;
+			}
+		}
+		return count;
 	}
 }
